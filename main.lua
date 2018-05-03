20963cf828c77d1b5b2e4cc9afe5a75bbe346627
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-05-03 10:45:18 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,21 @@
+
+-- Created by: Tristan Royer
+-- Date: 04 / 19 / 18
+-- Description: This calls the splash screen of the app to load itself.
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+display.setStatusBar(display.HiddenStatusBar)
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
+
+
